04337016ca78a80c7e62635e3d458d16f37100c8
libra guest <obrien.colin@gmail.com>
2012-06-30 12:39:39 -0400

    Updated Tests are accessable message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*jslint node:true plusplus:true vars:true nomen:true*/
 (function () {
     "use strict";
-    
+
     var testFiles = [
         "0_expects.js",
         "1_equality.js",
@@ -11,20 +11,20 @@
         "5_inheritance.js",
         "6_scope.js"
     ];
-    
+
     var express = require("express");
     var app = express.createServer();
-    
+
     var port = process.env.PORT || 3000;
     var ipaddr = process.env.IP || "localhost";
-    
+
     app.configure(function () {
         app.use(app.router);
         app.use(express["static"](__dirname + "/tests"));
         app.use(express["static"](__dirname + "/node_modules/mocha"));
         app.use(express["static"](__dirname + "/node_modules/expect.js"));
     });
-    
+
     app.get("/", function (req, res) {
         var i, ii;
         var html =
@@ -35,11 +35,11 @@
             "       <script src='expect.js'></script>" +
             "       <script src='mocha.js'></script>" +
             "       <script>mocha.setup('bdd')</script>";
-    
+
         for (i = 0, ii = testFiles.length; i < ii; i++) {
             html = html + "<script src='" + testFiles[i] + "'></script>";
         }
-    
+
         html = html +
             "   </head>" +
             "   <body>" +
@@ -47,14 +47,16 @@
             "       <script type='text/javascript'>mocha.run()</script>" +
             "   </body>" +
             "</html>";
-    
+
         res.end(html);
     });
-    
+
     app.listen(port, ipaddr);
-    
+
     if (process.env.PORT) {
-        console.log("Tests are now accessible");
+        console.log("****************************");
+        console.log("* Tests are now accessible *");
+        console.log("****************************");
     } else {
         console.log("Now listening at address http://" + ipaddr + ":" + port + "/");
     }
